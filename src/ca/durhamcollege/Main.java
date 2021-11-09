167bedf6a90633f2872cdcc9951165c4020ac8cf
Angaran Yogeswaran <angaran.y@gmail.com>
2021-11-09 16:45:52 -0500

    Included Basic Input and Output Test

--- src/ca/durhamcollege/Main.java
+++ src/ca/durhamcollege/Main.java
@@ -1,8 +1,24 @@
+/*
+    Author: Angaran Yogeswaran
+    Date: November 9th 2021
+ */
+
+
+
 package ca.durhamcollege;
 
-public class Main {
+import java.util.Scanner;
 
-    public static void main(String[] args) {
+public class Main
+{
+
+    public static void main(String[] args)
+    {
 	// write your code here
+        String name = "";
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Please enter your name: ");
+        name = keyboard.nextLine();
+        System.out.printf("You entered: %s",name);
     }
 }
